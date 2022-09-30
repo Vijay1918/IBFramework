8d1bfc6b12d1d9836dc920a2c6ae800b5398f688
Vijay1918 <vijayw1918@gmail.com>
2022-09-30 14:53:00 +0530

    Initial commit of cocoa pod

---
+++ Example/Pods/Target Support Files/IBFramework/IBFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IBFramework : NSObject
+@end
+@implementation PodsDummy_IBFramework
+@end
---
+++ Example/Pods/Target Support Files/Pods-IBFramework_Tests/Pods-IBFramework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IBFramework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_IBFramework_Tests
+@end
